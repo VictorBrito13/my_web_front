51cc7569577c8549ac0d9f38f25ed9654254e94c
Victor Brito <javibrito10@gmail.com>
2022-11-04 13:04:06 -0600

    responsive done

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  @ViewChild('menu') $menu:any
 
-  constructor() { }
+  constructor(
+    private renderer2: Renderer2
+  ) { }
 
   ngOnInit(): void {
   }
 
+  displayMenu(){
+    let close = this.$menu.nativeElement.classList.contains('menu-closed')
+
+    close ?
+    this.renderer2.removeClass(this.$menu.nativeElement, 'menu-closed') :
+    this.renderer2.addClass(this.$menu.nativeElement, 'menu-closed')
+  }
 }
